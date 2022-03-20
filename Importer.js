a451b757aad502de30a2f31516b26a9426b3172e
Noel Mace <contact@noelmace.com>
2022-03-20 18:27:53 +0100

    refactor: fix eslint issues

--- Importer.js
+++ Importer.js
@@ -6,10 +6,15 @@ import { createActionAuth } from "@octokit/auth-action";
 
 export class Importer {
   static LOG_SPACING_SIZE = 2;
+
   static LOG_BULLET_ITEM = "·️";
+
   static INPUT_SERVICE_ACCOUNT_JSON = "google-api-service-account-credentials";
+
   static INPUT_DOCUMENT_ID = "document-id";
+
   static INPUT_SHEET_NAME = "sheet-name";
+
   static INPUT_MODE = "mode";
 
   /**
@@ -24,14 +29,14 @@ export class Importer {
       const documentId = Core.getInput(Importer.INPUT_DOCUMENT_ID);
       const sheetName = Core.getInput(Importer.INPUT_SHEET_NAME);
       const mode = Core.getInput(Importer.INPUT_MODE) || "all";
-      Core.info("Running mode = " + mode);
+      Core.info(`Running mode = ${mode}`);
       if (!serviceAccountCredentials || !documentId || !sheetName) {
         throw new Error("🚨 Some Inputs missed. Please check project README.");
       }
       Core.info("Auth with GitHub Token...");
       const authGit = createActionAuth();
       const { token } = await authGit();
-      Core.info("Token: " + token);
+      Core.info(`Token: ${token}`);
       const octokit = new Octokit({
         auth: token,
       });
@@ -40,23 +45,24 @@ export class Importer {
       Core.endGroup();
 
       Core.startGroup("📑 Getting all Issues in repository...");
-      var page = 1;
-      var issuesData = [];
-      var issuesPage;
+      let page = 1;
+      let issuesData = [];
+      let issuesPage;
       do {
         Core.info(`Getting data from Issues page ${page}...`);
+        // eslint-disable-next-line no-await-in-loop
         issuesPage = await octokit.issues.listForRepo({
           owner: GitHub.context.repo.owner,
           repo: GitHub.context.repo.repo,
           state: "all",
-          page: page,
+          page,
         });
         Core.info(`There are ${issuesPage.data.length} Issues...`);
         issuesData = issuesData.concat(issuesPage.data);
         if (issuesPage.data.length) {
           Core.info("Next page...");
         }
-        page++;
+        page += 1;
       } while (issuesPage.data.length);
       Core.info("All pages processed:");
       issuesData.forEach((value) => {
@@ -72,7 +78,7 @@ export class Importer {
       });
       const sheets = google.sheets({
         version: "v4",
-        auth: auth,
+        auth,
       });
       Core.info("Done.");
       Core.endGroup();
@@ -85,53 +91,44 @@ export class Importer {
       Core.info("Done.");
       Core.endGroup();
 
-      Core.startGroup(`🔨 Form Issues data for Sheets format...`);
-      Core.info("Count issues = " + issuesData.length);
+      Core.startGroup("🔨 Form Issues data for Sheets format...");
+      Core.info(`Count issues = ${issuesData.length}`);
 
-      issuesData;
-      var issueSheetsData = [];
+      const issueSheetsData = [];
+      // eslint-disable-next-line no-restricted-syntax
       for (const value of issuesData) {
-        if (mode == "issues") {
-          if (value.pull_request) {
-            continue;
-          }
+        if (
+          mode === "all" ||
+          (mode === "issues" && !value.pull_request) ||
+          (mode === "milestone_issues" &&
+            value.milestone &&
+            value.milestone.state === "open" &&
+            !value.pull_request)
+        ) {
+          const labels = value.labels.map((label) => label.name);
+
+          const assignees = value.assignees.map((assignee) => assignee.login);
+
+          issueSheetsData.push([
+            value.number,
+            value.state,
+            value.pull_request ? "Pull Request" : "Issue",
+            value.title,
+            value.html_url,
+            Object.keys(labels)
+              .map((k) => labels[k])
+              .join(", "),
+            Object.keys(assignees)
+              .map((k) => assignees[k])
+              .join(", "),
+            value.milestone?.title,
+            value.milestone?.state,
+            value.milestone?.due_on,
+            value.milestone?.html_url,
+            value.body,
+            value.closed_at,
+          ]);
         }
-        if (mode == "milestone_issues") {
-          if (
-            !value.milestone ||
-            value.milestone.state != "open" ||
-            value.pull_request
-          ) {
-            continue;
-          }
-        }
-        var labels = [];
-        for (const label of value.labels) {
-          labels.push(label.name);
-        }
-        var assignees = [];
-        for (const assignee of value.assignees) {
-          assignees.push(assignee.login);
-        }
-        issueSheetsData.push([
-          value.number,
-          value.state,
-          value.pull_request ? "Pull Request" : "Issue",
-          value.title,
-          value.html_url,
-          Object.keys(labels)
-            .map((k) => labels[k])
-            .join(", "),
-          Object.keys(assignees)
-            .map((k) => assignees[k])
-            .join(", "),
-          value.milestone?.title,
-          value.milestone?.state,
-          value.milestone?.due_on,
-          value.milestone?.html_url,
-          value.body,
-          value.closed_at,
-        ]);
       }
       issueSheetsData.forEach((value) => {
         Core.info(`${Importer.LOG_BULLET_ITEM} ${JSON.stringify(value)}`);
@@ -142,11 +139,11 @@ export class Importer {
       Core.info("Adding header...");
       await sheets.spreadsheets.values.append({
         spreadsheetId: documentId,
-        range: sheetName + "!A1:1",
+        range: `${sheetName}!A1:1`,
         valueInputOption: "USER_ENTERED",
         requestBody: {
           majorDimension: "ROWS",
-          range: sheetName + "!A1:1",
+          range: `${sheetName}!A1:1`,
           values: [
             [
               "#",
@@ -167,14 +164,14 @@ export class Importer {
         },
       });
       Core.info("Appending data...");
-      Core.info("Count appending issues = " + issueSheetsData.length);
+      Core.info(`Count appending issues = ${issueSheetsData.length}`);
       await sheets.spreadsheets.values.append({
         spreadsheetId: documentId,
-        range: sheetName + "!A1:1",
+        range: `${sheetName}!A1:1`,
         valueInputOption: "USER_ENTERED",
         requestBody: {
           majorDimension: "ROWS",
-          range: sheetName + "!A1:1",
+          range: `${sheetName}!A1:1`,
           values: issueSheetsData,
         },
       });
